99a9123ce6371b250b5e9de3a0166fa0ec2f179e
r0man <roman@burningswell.com>
2017-01-08 14:22:01 +0100

    Redefine console.error to console.log

--- test/sablono/test.cljs
+++ test/sablono/test.cljs
@@ -6,6 +6,10 @@
             [sablono.server-test]
             [sablono.util-test]))
 
+;; Or doo will exit with an error, see:
+;; https://github.com/bensu/doo/issues/83#issuecomment-165498172
+(set! (.-error js/console) (fn [x] (.log js/console x)))
+
 (doo-tests 'sablono.core-test
            'sablono.interpreter-test
            'sablono.normalize-test
